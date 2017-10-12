ac29ae53ef2ccb91565a402f814acf38e403889f
dawid621 <dawid621@o2.pl>
2017-10-12 19:23:59 +0200

    Update

---
+++ MeshAnimatorProblem/Assets/Script/SkinnedEthanRun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinnedEthanRun : MonoBehaviour {
+
+  private Animator anim;
+
+
+	// Use this for initialization
+	void Start ()
+  {
+    anim = GetComponent<Animator>();
+    anim.applyRootMotion = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    anim.SetFloat("Forward", 1);
+	}
+}
